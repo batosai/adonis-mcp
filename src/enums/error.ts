3d397f804a8407e487f05506becd7333a96a965b
Jeremy Chaufourier <jeremy@chaufourier.fr>
2025-11-13 00:52:09 +0100

    feat: add error enum

---
+++ src/enums/error.ts
@@ -0,0 +1,19 @@
+/**
+ * @jrmc/adonis-mcp
+ *
+ * @license MIT
+ * @copyright Jeremy Chaufourier <jeremy@chaufourier.fr>
+ */
+
+export enum ErrorCode {
+  // SDK error codes
+  ConnectionClosed = -32000,
+  RequestTimeout = -32001,
+
+  // Standard JSON-RPC error codes
+  ParseError = -32700,
+  InvalidRequest = -32600,
+  MethodNotFound = -32601,
+  InvalidParams = -32602,
+  InternalError = -32603
+}
